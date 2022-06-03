be7240c910f52d0c1f6c674976cdbb1e8fff347c
Veronika1007 <veronika.horatschek@gmail.com>
2022-06-03 23:01:44 +0100

    added github footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">Weather App</header>
+      <footer>
+        This project was coded by Veronika and is{" "}
+        <a
+          href="https://github.com/Veronika1007/my-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on github
+        </a>
+      </footer>
     </div>
   );
 }
